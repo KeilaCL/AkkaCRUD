07a3e0bd19f91840382811c964ed13e7d8e7bc6e
Keila Cortéz <50417086+KeilaCL@users.noreply.github.com>
2023-02-12 09:53:41 -0600

    arreglos puerto

--- AkkaCRUD/AkkaCRUD/Program.cs
+++ AkkaCRUD/AkkaCRUD/Program.cs
@@ -9,9 +9,22 @@ namespace AkkaCRUD
         {
             var system = ActorSystem.Create("demo-system");
             var userActor = system.ActorOf<UserActor>("user-actor");
-            var httpActor = system.ActorOf(Props.Create(() => new HttpActor(userActor)), "http-actor");
-            Console.ReadLine();
-            system.Terminate().Wait();
+
+            var httpActor = system.ActorOf(Props.Create(() => new HttpActor(userActor)), "HttpActor");
+
+            using (var listener = new HttpListener())
+            {
+                listener.Prefixes.Add("http://localhost:8080/");
+                listener.Start();
+
+                while (true)
+                {
+                    var context = listener.GetContext();
+
+                    // Enviar la solicitud al actor HttpActor
+                    httpActor.Tell(context);
+                }
+            }
 
         }
     }
